31c5bb1d2c5e8c0a992b2e831cd8bf82b4f85d4b
Herb Miller <herb@bobbingwide.com>
2016-02-25 12:55:43 +0000

    Add Issue #17 - Improve performance by operating locally

---
+++ admin/oik-create-apis.php
@@ -0,0 +1,291 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * Lazy implementation of "run_oik-shortcodes.php" 
+ * to perform createapi2.php against the local database
+ * 
+ * Uses _local versions of the functions from oik-batch/creatapi2.php
+ * 
+ *
+ */
+function oiksc_lazy_run_oik_shortcodes() {
+	
+	echo "Running creatapi2 directly" . PHP_EOL;
+
+  oik_require( "admin/oik-apis.php", "oik-shortcodes" );
+	oik_require( "oik-ignore-list.php", "oik-batch" );
+	oik_require( "oik-login.inc", "oik-batch" );
+	
+
+  //$component = "allow-reinstalls";
+	//$previous = null;
+	//$start = null;
+	
+	$component = bw_array_get( $_SERVER['argv'], 1, "allow-reinstalls" );
+	$previous = bw_array_get( $_SERVER['argv'], 2, null );
+	$start = bw_array_get( $_SERVER['argv'], 3, 1 );
+	
+	//bw_trace2( $_SERVER, "_SERVER" );  
+	$components = bw_as_array( $component );
+	
+	oiksc_preloader(); 
+	
+	//foreach ( $components as $component ) {
+		_ca_doaplugin_local( $component, $previous, $start );
+	//} 
+		
+		
+}
+
+/**  
+ * Create the APIs for a component
+ *
+ * List the files for the component
+ * Components supported:
+ * "wordpress" - all files in the WordPress installation excluding wp-content
+ * <i>plugin</i> - all the files in the named plugin 
+ * <i>theme</i> - all the files in the named theme
+ *
+ * Process each file in the component, defining the file, classes, methods and APIs
+ * 
+ * @param string $component - the name of the plugin or theme, or "wordpress"
+ * @param string $previous - the previous version to compare against - for performance
+ * @param string $start 
+ */
+function _ca_doaplugin_local( $component, $previous=null, $start=1 ) {
+  global $plugin;
+	$plugin = $component;
+  if ( $plugin ) {
+    $component_type = oiksc_query_component_type( $plugin );
+    if ( $component_type ) {
+      echo "Doing a $component_type: " . $plugin . PHP_EOL;
+  
+      $response = oikb_get_response( "Continue?", true );
+      if ( $response ) {
+        oik_require( "admin/oik-apis.php", "oik-shortcodes" );
+        //wp_register_plugin_realpath( WP_PLUGIN_DIR . "/$plugin/." );
+        oik_require( "oik-list-previous-files.php", "oik-batch" );
+				
+				$files = oikb_list_changed_files( $previous, $plugin, $component_type );
+	
+				if ( null === $files ) {
+					$files = oiksc_load_files( $plugin, $component_type );
+					$files = oikb_maybe_do_files( $files, $previous, $component, $component_type );
+				}
+				oiksc_pre_load_component( $plugin, $component_type );
+        oiksc_do_files( $files, $plugin, $component_type, "_ca_dofile_local", $start );
+      }
+    } else {
+      echo "Invalid plugin/theme: $component" . PHP_EOL;
+    }       
+  } else {
+    echo "Missing --plugin= parameter" . PHP_EOL;
+  } 
+}
+
+
+/**
+ * Process a file
+ *
+ * We process .php and .inc files
+ * Note: WordPress doesn't have any .inc files
+ *
+ * @TODO: Expand to cover ALL files.
+ *
+ * @param string $file - the file name
+ *
+ */ 
+function _ca_dofile_local( $file, $plugin, $component_type, $start=1 ) {
+  if ( $plugin ) {
+    $inignorelist = _la_checkignorelist( $file );
+  } else {
+    $inignorelist = false;
+  }
+  if ( !$inignorelist ) {    
+    echo "Processing: $file". PHP_EOL;
+    $ext = pathinfo( $file, PATHINFO_EXTENSION );
+    $ext = strtolower( $ext );
+    $exts = bw_assoc( array( "php", "inc" ));
+    $validext = bw_array_get( $exts, $ext, false );
+    if ( $validext ) {
+      _lf_dofile_local( $file, $plugin, $component_type );
+      _ca_doapis_local( $file, $plugin, $component_type, $start ); 
+    }
+  }  
+}
+
+
+/**
+ * Create the apis for a particular file in a plugin
+ * 
+ * 
+ * Obtain a list of the APIs in the source file. 
+ * For each API send a message to the server to create/update the API.
+ * Note: We ignore classes - these are created dynamically
+ *
+ *
+ * @param string $file - file name to load
+ * @global $plugin - which may be null when processing WordPress core
+ */
+function _lf_dofile_local( $file, $plugin, $component_type ) {
+  //global $plugin;
+  //if ( oikb_get_site() ) {
+    if ( $plugin == "wordpress" ) {
+      $file = strip_directory_path( ABSPATH, $file );
+    } else {
+      echo $file . PHP_EOL;
+        
+    }
+    echo "Processing file: $plugin,$file" . PHP_EOL;
+    $response = oikb_get_response( "Continue to process file?" );
+    if ( $response ) {
+      //$apikey = oikb_get_apikey();
+      //oik_require( "oik-admin-ajax.inc", "oik-batch" );
+      //oikb_admin_ajax_post_create_file( oikb_get_site(), $plugin, $file, $apikey );
+			
+			oiksc_local_oiksc_create_file( $plugin, $file, $component_type );
+			$discard = bw_ret();
+    }
+    echo "Processing file: $plugin,$file ended" . PHP_EOL;
+		
+  //}
+}
+
+
+
+/**
+ * Create the apis for a particular file in a plugin
+ * 
+ * 
+ * Obtain a list of the APIs in the source file. 
+ * For each API send a message to the server to create/update the API.
+ * Note: We ignore classes - these are created dynamically
+ *
+ *
+ * @param string $file - file name to load
+ * @global $plugin - which may be null when processing WordPress core
+ */
+function _ca_doapis_local( $file, $plugin_p, $component_type, $start=1 ) {
+  global $plugin;
+  $plugin = $plugin_p;
+  static $count = 0;
+  echo "Processing valid: $plugin $file $component_type" . PHP_EOL;
+  $apis = _oiksc_get_apis2( $file, true, $component_type );
+  foreach ( $apis as $api ) {
+    $count++;
+		if ( $count < $start ) {
+			continue;
+		}
+		
+		$apiname = $api->getApiName();
+		
+		echo "Processing API: $apiname, $file, $plugin, $component_type " . PHP_EOL;
+    $response = _ca_checkforselected_api( $apiname, $count );
+    if ( $response ) {
+      $response = oikb_get_response( "Continue to create API?" );
+    }  
+    if ( $response ) {
+			oiksc_local_oiksc_create_api( $plugin, $file, $component_type, $api );
+			$discard = bw_ret();
+    }  
+    
+  }
+}
+
+ 
+ 
+/**
+ * Check for the selected API
+ */
+function _ca_checkforselected_api( $api, $count ) {
+  global $selected_api;
+  if ( $selected_api ) {
+    $selected = ( $api == $selected_api ) || ( $count >= $selected_api );
+  } else {
+    $selected = true; 
+  }
+  return( $selected );
+}
+
+
+/**
+ * Preload the libraries used in create+file and create_apui
+ */
+function oiksc_preloader() {
+  do_action( "oik_loaded" );
+	oiksc_autoload();
+	
+	
+	oik_require( "includes/bw_posts.inc" ); 
+	oik_require( "admin/oik-apis.php", "oik-shortcodes" );
+	oik_require( "admin/oik-files.php", "oik-shortcodes" );
+	oik_require( "admin/oik-shortcodes.php", "oik-shortcodes" );
+	oik_require( "feed/oik-plugins-feed.php", "oik-plugins" );
+	oik_require( "shortcodes/oik-api-importer.php", "oik-shortcodes" );
+	
+	
+
+}
+
+function oiksc_pre_load_component( $plugin, $component_type, $force=false ) {
+	global $plugin_post;
+ 	//if ( !$plugin_post ) {
+		$plugin_post = oiksc_load_component( $plugin, $component_type );
+  //}
+	return( $plugin_post );
+}	
+ 
+
+/**
+ * Create an oik_file locally
+ *
+ * Create or update an "oik_file" post AND parse all the classes, methods and APIs implemented, including the main file,
+ * using logic similar to createapi2.php
+ * 
+ */
+function oiksc_local_oiksc_create_file( $plugin, $file, $component_type ) {
+  global $plugin_post;
+	echo "Processing: $file, $plugin, $component_type " . PHP_EOL;
+	
+  if ( is_null( $plugin )) {
+    $plugin = 'wordpress';
+    //$file = ABSPATH . $file;
+    //$file = str_replace( "\\", "/", $file );
+    bw_trace2( $file, "file" );
+  } 
+  // $plugin_post = oikp_load_plugin( $plugin );
+  $file_id = _oikai_create_file( $plugin_post->ID, $file ); 
+  $filename = oik_pathw( $file, $plugin, $component_type );
+  $parsed_source = oiksc_display_oik_file( $filename, $component_type, $file_id, true );
+}
+
+/**
+ * Create or update an oik_api locally
+ *
+ * The API may contain the API name in the format of 
+ *  'class::api' - representing a method
+ *  'api' - representing a function
+ *  'class::' - representing a class
+ * 
+ * The value we store in "_oik_api_name" for a method will be in the class::api format 
+ * 
+ * API names are normally expected to be unique. This code does not cater for use of namespacing. 
+ * 
+ */
+function oiksc_local_oiksc_create_api( $plugin, $file, $component_type, $api_object ) {
+	global $plugin_post;
+	$api = $api_object->getApiName();	
+	$type = $api_object->getApiType();
+	$title = $api_object->getShortDescription();
+	if ( $plugin_post ) {
+		$func = oikai_get_func( $api, null ); 
+		if ( $func ) {
+			$post_id = _oiksc_create_api( $plugin_post->ID, $api, $file, $type, $title ); 
+		} else {
+			$post_id = oikai_get_classref( $api, null, $plugin_post->ID, $file );
+		}
+	} else {
+		e( "Invalid plugin: $plugin ");
+	}    
+}
+
