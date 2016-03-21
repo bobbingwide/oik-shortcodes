bc06bd6826c5d6df11cd5a0faeffa3e132361889
Herb Miller <herb@bobbingwide.com>
2016-03-21 13:24:17 +0000

    Issue #18 - improve performance with pragmatic links for [hook] shortcode

--- shortcodes/oik-hook.php
+++ shortcodes/oik-hook.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2014
+<?php // (C) Copyright Bobbing Wide 2014-2016
 
 /**
  * Return the hook type for this hook
@@ -28,36 +28,79 @@ function oikho_query_hook_type( $post_id ) {
  *
  */
 function oikho_hook( $atts=null, $content=null, $tag=null ) {
-  $hooks = bw_array_get_from( $atts, "hook,0", null );
-  if ( $hooks ) {
-    $hooka = bw_as_array( $hooks );
-    //oik_require( "shortcodes/oik-apilink.php", "oik-shortcodes" );
-    oik_require( "shortcodes/oik-hookslink.php", "oik-shortcodes" );
-    $count = 0;
-    foreach ( $hooka as $key => $hook ) {
-      //$api = oikai_simplify_apiname( $api );
-      if ( $count ) {
-        e( "," );
-        e( "&nbsp;" );
-      }
-      $count++;
-      //$type = oikai_determine_reference_type( $hook ); 
-      //oikai_handle_reference_type( $hook, $type );
-      $posts = oikho_get_oik_hooks_byname( $hook );
-      if ( $posts ) {
-        foreach ( $posts as $post ) {
-          alink( null, get_permalink( $post->ID), $hook );
-          $hook_type = oikho_query_hook_type( $post->ID );
-          e( " <i>$hook_type</i>" );
-          
-        }
-      } else {
-        e( $hook );
-      }
-         
-    } 
+	$hooks = bw_array_get_from( $atts, "hook,0", null );
+	$type = bw_array_get( $atts, 1, null );
+	$num_args = bw_array_get( $atts, 2, null );
+	$count = bw_array_get( $atts, 3, null ); 
+	if ( $hooks ) {
+		if ( $type && $count ) {
+			oikho_pragmatic_link( $hooks, $type, $num_args, $count );
+		} else {
+			$hooka = bw_as_array( $hooks );
+			//oik_require( "shortcodes/oik-apilink.php", "oik-shortcodes" );
+			oik_require( "shortcodes/oik-hookslink.php", "oik-shortcodes" );
+			$count = 0;
+			foreach ( $hooka as $key => $hook ) {
+				//$api = oikai_simplify_apiname( $api );
+				if ( $count ) {
+					 e( "," );
+					 e( "&nbsp;" );
+				}
+				$count++;
+				//$type = oikai_determine_reference_type( $hook ); 
+				//oikai_handle_reference_type( $hook, $type );
+				$posts = oikho_get_oik_hooks_byname( $hook );
+				if ( $posts ) {
+					foreach ( $posts as $post ) {
+						alink( null, get_permalink( $post->ID), $hook );
+						$hook_type = oikho_query_hook_type( $post->ID );
+						e( " <i>$hook_type</i>" );
+						
+					}
+				} else {
+					e( $hook );
+				}
+			}
+		}
   } else {
     // Don't do anything for a missing hook name ... or implements [hooks] ?
   }
   return( bw_ret() );
 }
+
+/**
+ * Create a pragmatic link for a hook
+ * 
+ * [hook genesis_pre action 0 1]
+ * [hook genesis_init action 0 1]
+ * [hook genesis_init;pre_option_template filter 2 3]
+ *
+ * 
+ *
+ * @param string $hooks
+ * @param string $type action or filter
+ * @param integer $num_args 
+ * @param integer $count
+ */
+function oikho_pragmatic_link( $hooks, $type, $num_args, $count ) {
+	$hooka = explode( ";", $hooks );
+	$hook = end( $hooka );
+	$url = site_url( "oik_hook/" . $hook ); 
+	sdiv( "hook" );
+	$copies = ( count( $hooka )-1) * 2;
+	span( "indent-$copies" );
+	e( str_repeat( "&nbsp;", $copies ) );
+	epan();
+	alink( "hooklink", $url, "$hook - $type" );
+	//sepan( "sep" );
+	span( "num_args" );
+	e( $num_args );
+	epan();
+	//sepan( "sep" );
+	span( "count" );
+	e( $count );
+	epan();
+	ediv();
+}  
+
+
