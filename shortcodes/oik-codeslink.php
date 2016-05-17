c99ff89be1c3135df1205577673a09ed5e7040a9
Herb Miller <herb@bobbingwide.com>
2016-05-17 11:50:07 +0100

    Fixes #27 - add component parameter to [codes] to support child themes

--- shortcodes/oik-codeslink.php
+++ shortcodes/oik-codeslink.php
@@ -2,61 +2,67 @@
 
 /** 
  * Automagically determine the shortcode list
- * If no functions have been passed then we can determine the list of APIs from the context of the current post
- 
- * post_type      action
- * ----------     -------------
- * oik_shortcode  n/a
- * oik-plugins    find ALL the shortcodes linked to the plugin through "_oik_sc_plugin"
- * oik-themes     find ALL the shortcodes linked to the theme through "_oik_sc_plugin"
- * oik_api        find all the shortcodes linked to the API through "_oik_sc_func"
- * other          n/a
  * 
+ * If no functions have been passed then we can determine the list of APIs
+ * either using the component ID or the context of the current post.
  * 
-      $id = get_post_meta( $post->ID, "_oik_sc_func", true );
-      $atts = array();
-      $atts['post_type'] = "oik_shortcode";
-      $atts['include'] = $id;
+ * The component parameter was added to support displaying shortcodes for a Template ( parent ) theme
+ * 
+ * post_type     | action
+ * ----------    | -------------
+ * oik_shortcode | n/a
+ * oik-plugins   | find ALL the shortcodes linked to the plugin through "_oik_sc_plugin"
+ * oik-themes    | find ALL the shortcodes linked to the theme through "_oik_sc_plugin"
+ * oik_api       | find all the shortcodes linked to the API through "_oik_sc_func"
+ * other         | n/a
+ * 
+ * @param array $atts shortcode parameters
  */
 function oikai_listcodes( $atts ) {
-  global $post;
-  if ( $post ) {
-    oik_require( "shortcodes/oik-list.php" ); 
-    if ( $post->post_type == "oik_api" ) {
-      $atts['post_type'] = "oik_shortcodes"; 
-      $atts['meta_key' ] = "_oik_sc_func";
-      $atts['meta_value'] = $post->ID;
-      e( bw_list( $atts ) );
-    } elseif ( $post->post_type == "oik-plugins" ) {
-      $atts['post_type'] = "oik_shortcodes"; 
-      $atts['meta_key' ] = "_oik_sc_plugin";
-      $atts['meta_value'] = $post->ID;
-      e ( bw_list( $atts ) );
-    } elseif ( $post->post_type == "oik-themes" ) {
-      $atts['post_type'] = "oik_shortcodes"; 
-      $atts['meta_key' ] = "_oik_sc_plugin";
-      $atts['meta_value'] = $post->ID;
-      e ( bw_list( $atts ) );
-    } else {
-      // Not the right post_type to list APIs
-      bw_trace2( $post->post_type, "Unexpected post_type" );
-      p( "Unexpected post type: " .  $post->post_type );
-    }
-    
-  } else { 
-    // Don't expect this! 
-  }
-  
+	oik_require( "shortcodes/oik-list.php" ); 
+	$post_id = bw_array_get( $atts, "component", null );
+	if ( $post_id ) {
+		$atts['post_type'] = "oik_shortcodes"; 
+		$atts['meta_key' ] = "_oik_sc_plugin";
+		$atts['meta_value'] = $post_id;
+		e( bw_list( $atts ) );
+	} else {
+		global $post;
+		if ( $post ) {
+			$post_id = $post->ID;
+			if ( $post->post_type == "oik_api" ) {
+				$atts['post_type'] = "oik_shortcodes"; 
+				$atts['meta_key' ] = "_oik_sc_func";
+				$atts['meta_value'] = $post_id;
+				e( bw_list( $atts ) );
+			} elseif ( $post->post_type == "oik-plugins" ) {
+				$atts['post_type'] = "oik_shortcodes"; 
+				$atts['meta_key' ] = "_oik_sc_plugin";
+				$atts['meta_value'] = $post_id;
+				e ( bw_list( $atts ) );
+			} elseif ( $post->post_type == "oik-themes" ) {
+				$atts['post_type'] = "oik_shortcodes"; 
+				$atts['meta_key' ] = "_oik_sc_plugin";
+				$atts['meta_value'] = $post_id;
+				e ( bw_list( $atts ) );
+			} else {
+				// Not the right post_type to list APIs
+				bw_trace2( $post->post_type, "Unexpected post_type" );
+				p( "Unexpected post type: " .  $post->post_type );
+			}
+		} else { 
+			// Don't expect this! 
+		}
+	}
 }
 
 /**
  * Load the shortcodes listed in the array
+ *
+ * Since the plugin/theme name is not specified we could get duplicates in the results.
  * 
  * @param array $shortcodes - array of shortcodes 
  * @result array $posts - array of posts found
- * 
- * Since the plugin name is not specified we could get duplicates in the results.
- * 
  */
 function oiksc_get_shortcodes_bycode( $shortcodes ) {
   oik_require( "includes/bw_posts.inc" );
@@ -86,17 +92,17 @@ function oiksc_get_shortcodes_bycode( $shortcodes ) {
  * How about? 
  * [codes "bbboing_sc fiddle" ]  
  * 
- *
+ * @param array $atts - shortcode parameters
+ * @param string $content - shortcode content - not expected
+ * @param string $tag - shortcode name
+ * @return string generated HTML 
  */
-function oikai_codeslink( $atts=null, $hmm, $tag ) {
-  // $atts[] = bw_array_get( $atts, "codes", 
-  //$shortcode = bw_array_get( $atts, 0, null );
+function oikai_codeslink( $atts=null, $content, $tag ) {
   $shortcode = bw_array_get( $atts, "codes", null );
   $shortcodes = bw_as_array( $shortcode );
   $unkeyed = bw_array_get_unkeyed( $atts );
   $shortcodes = array_merge( $shortcodes, $unkeyed );
-  
-  bw_trace2( $shortcodes, "shortcodes" );
+  bw_trace2( $shortcodes, "shortcodes", true, BW_TRACE_DEBUG );
   
   if ( count( $shortcodes) ) {
     oik_require( "admin/oik-shortcodes.php", "oik-shortcodes" );
@@ -124,6 +130,7 @@ function codes__help() {
 
 function codes__syntax( $shortcode="codes" ) {  
   $syntax = array( "codes" => bw_skv( null, "<i>shortcode</i>", "Shortcode name list e.g. bw_pages" )
+								 , "component" => bw_skv( null, "<i>ID</i>", "ID of plugin/theme for the shortcodes" )
                  );
   $syntax += _sc_classes();
   return( $syntax );
